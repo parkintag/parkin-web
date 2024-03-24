cab49e1611aea844952017de80e9f96f863760f0
ParkIN <160492672+parkintag@users.noreply.github.com>
2024-03-24 08:36:41 +0100

    Update ga.js

--- js/ga.js
+++ js/ga.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDo2JPDgTJzhG7CgI85-axV0VU16Q1YVV4",
-  authDomain: "parkin-414214.firebaseapp.com",
-  projectId: "parkin-414214",
-  storageBucket: "parkin-414214.appspot.com",
-  messagingSenderId: "550761066079",
-  appId: "1:550761066079:web:d01faf40b1e295948e75ea",
-  measurementId: "G-3SFQ0YNDYV"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDo2JPDgTJzhG7CgI85-axV0VU16Q1YVV4",
+    authDomain: "parkin-414214.firebaseapp.com",
+    projectId: "parkin-414214",
+    storageBucket: "parkin-414214.appspot.com",
+    messagingSenderId: "550761066079",
+    appId: "1:550761066079:web:d01faf40b1e295948e75ea",
+    measurementId: "G-3SFQ0YNDYV"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
